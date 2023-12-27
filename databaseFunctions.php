082580b1ae49eb212150a7c8e92dffa1de3d8567
Karolina <karolina.limanowska@gmail.com>
2023-12-27 21:55:33 +0100

    working on a booking function

--- databaseFunctions.php
+++ databaseFunctions.php
@@ -20,14 +20,34 @@ function changeRoomPrice($roomId, $price){
 
 }
 
-function createBooking ($guestName, $guestSurname, $arrivalDate, $departureDate){
+function createBooking ($guestName, $guestSurname, $arrivalDate, $departureDate, $bookingId, $featuresIdArray){
     $db = connect('hotel.sqlite3');
-    $query = $db->query("INSERT INTO Bookings (GuestName, GuestSurname, ArrivalDate, DepartureDate) VALUES ( '$guestName', '$guestSurname', '$arrivalDate', '$departureDate')");
+
+    $query = $db->query(
+        "INSERT INTO Bookings (GuestName, GuestSurname, ArrivalDate, DepartureDate) 
+        VALUES ('$guestName', '$guestSurname', '$arrivalDate', '$departureDate')");
+    $bookingId = $db->lastInsertId(); //gets the ID of the lasy booking 
+
+
+    if(count($featuresIdArray)>0) 
+
+    $bookedFeaturesString = "";
+
+    foreach($featuresIdArray as $featureId){
+		$bookedFeaturesString .= "(".$bookingId.",".$featureId."),"; 
+		// .= glues together the left and the right string
+	}
+
+    $query = $db->query(
+        "INSERT INTO BookedFeatures (BookingId, FeatureId) VALUES ". substr($bookedFeaturesString, 0, -1));
+
+
 }
 
+createBooking('Marta', 'Kowalska', '2024-01-05', '2024-01-06','' , 1);
 
-$hotel = getHotel();
 
-createBooking ('Karolina', 'Limanowska', '2024-01-15', '2024-01-17');
+
+
 
 ?>
