45575fe20003845ffa130935b1db471037b0787d
Karolina <karolina.limanowska@gmail.com>
2024-01-07 19:28:25 +0100

    working on footer

--- footer.php
+++ footer.php
@@ -1,5 +1,23 @@
 <footer class="footer">
-                <h5>Contact info</h5>
+    <div class="phoneNumber">
+        <h5>Contact us</h5>
+            <p>Phone: +62 123 456 7890</p>
+    </div>
+    <div class="adress">
+        <h5>Pay us a visit</h5>
+            <p>Sahaja Resort</br>
+            Jalan Harmoni No. 88</br>
+            Ubud, Central Java</br>
+            Indonesia</p>
+    </div>
+    <div class="checkInHours">
+        <h5>Check-in hours</h5>
+            <p>12:00-12:00</p>
+    </div>
+    <div class="newsletter">
+        <h5>Join our newsletter</h5>
+    </div>
+
             </footer>
         </div>    
     </body>
